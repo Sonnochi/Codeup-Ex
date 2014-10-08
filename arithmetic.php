ad852e243b61da39af540d1ee38be4e0e42ed313
Josee LeChele Farmer <Jfarmer@Josees-MacBook-Pro.local>
2014-10-08 09:37:10 -0500

    Function Exercise

---
+++ arithmetic.php
@@ -0,0 +1,29 @@
+<?php
+
+function add($a, $b) {
+    echo $a + $b . PHP_EOL;
+    // Add code here
+}
+add(8, 18);
+function subtract($a, $b) {
+	echo $a - $b . PHP_EOL;
+    // Add code here
+}
+subtract(8, 18);
+
+function multiply($a, $b) {
+    // Add code here
+    echo $a * $b . PHP_EOL;
+}
+multiply(8, 18);
+function divide($a, $b) {
+    // Add code here
+    echo $a / $b . PHP_EOL;
+}
+divide(8, 18);
+
+function modulus($a, $b) {
+	//Add code here
+	echo $a % $b . PHP_EOL;
+}
+modulus(8, 18);
