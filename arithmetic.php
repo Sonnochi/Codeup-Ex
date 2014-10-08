a0256a46897c1c7ea7a7a25a4b421ecd86a2e309
Josee LeChele Farmer <Jfarmer@Josees-MacBook-Pro.local>
2014-10-08 10:36:29 -0500

    Arithmetic function exercise

--- arithmetic.php
+++ arithmetic.php
@@ -1,29 +1,46 @@
 <?php
-
 function add($a, $b) {
-    echo $a + $b . PHP_EOL;
-    // Add code here
+    if (is_numeric($a) && is_numeric($b)) {
+        echo $a + $b . PHP_EOL;
+    } else {
+        echo "ERROR: Both $a and $b must be numbers" . PHP_EOL;
+    }
 }
 add(8, 18);
+add(dog, cat);
 function subtract($a, $b) {
-	echo $a - $b . PHP_EOL;
-    // Add code here
+    if (is_numeric($a) && is_numeric($b)) {
+        echo $a - $b . PHP_EOL;
+    } else {
+        echo "ERROR: Both $a and $b must be numbers" . PHP_EOL;
+    }	
 }
 subtract(8, 18);
-
+subtract(lion, tiger);
 function multiply($a, $b) {
-    // Add code here
-    echo $a * $b . PHP_EOL;
+	    if (is_numeric($a) && is_numeric($b)) {
+        echo $a * $b . PHP_EOL;
+    } else {
+        echo "ERROR: Both $a and $b must be numbers" . PHP_EOL;
+    }
 }
 multiply(8, 18);
-function divide($a, $b) {
-    // Add code here
-    echo $a / $b . PHP_EOL;
+multiply(rabbit, rabbit);
+function divide($a, $b){
+	    if (is_numeric($a) && is_numeric($b)) {
+        echo (int)($a / $b) . PHP_EOL;
+    } else {
+        echo "ERROR: Both $a and $b must be numbers" . PHP_EOL;
+    }
 }
-divide(8, 18);
-
-function modulus($a, $b) {
-	//Add code here
-	echo $a % $b . PHP_EOL;
+divide(80, 18);
+divide(cell, cell);
+function modulus($a, $b){
+	    if (is_numeric($a) && is_numeric($b)) {
+        echo $a % $b . PHP_EOL;
+    } else {
+        echo "ERROR: Both $a and $b must be numbers" . PHP_EOL;
+    }
 }
 modulus(8, 18);
+modulus(bears, trees);
